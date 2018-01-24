26fe879b2ace6847b5d567c7b840ee1c27d1adb5
Máté Jónás <kornjoni@gmail.com>
2018-01-24 19:03:56 +0100

    section 3 assignment
    the app sums two numbers from the args[]

---
+++ sumtwonumbersassigment/src/main/java/com/homeye/App.java
@@ -0,0 +1,15 @@
+package com.homeye;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println(a + b);
+    }
+}
